efb64c1a59f16792bc06a2d64b83ce5ea9f547e1
Nikolai Li <nikolaihcoe@gmail.com>
2024-11-20 13:25:27 +0100

    Rabbit class begyndelse

--- src/Rabbit.java
+++ src/Rabbit.java
@@ -1,2 +1,11 @@
-public class Rabbit {
+import itumulator.simulator.Actor;
+import itumulator.world.World;
+import itumulator.world.Location;
+import java.util.*;
+
+public class Rabbit implements Actor{
+    @Override
+    public void act(World world) {
+        Random r = new Random();
+    }
 }
