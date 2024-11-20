ac2a72634df21e2f554ed7e58cf99766f45e2f2e
Tore Haugan Engberg <teng@itu.dk>
2024-11-20 14:43:07 +0100

    Update Grass.java
    Added pseudocode and method for the class

--- grpro_project/src/Grass.java
+++ grpro_project/src/Grass.java
@@ -1,2 +1,43 @@
+import itumulator.executable.Program;
+import itumulator.world.Location;
+import itumulator.world.World;
+
+import java.util.*;
+
 public class Grass {
+    Random random = new Random();
+
+    public void grow(World world){
+        Set<Location> neighbours = world.getSurroundingTiles();
+        List<Location> list = new ArrayList<>(neighbours);
+
+        if(!list.isEmpty()){
+            Location location = list.get(random.nextInt(list.size()));
+
+            if(world.containsNonBlocking(location)){
+                world.setTile(location, this);
+                spread(world, location);
+            }
+        } else {
+            System.out.println("Nothing to grow");
+        }
+    }
+
+    /**
+     * A method that randomly spreads grass objects around the given parameter.
+     * The method has a 25% chance of spreading grass after each step
+     * @param world
+     * @param location
+     */
+    public void spread(World world, Location location){
+        Set<Location> surroundingTiles = world.getSurroundingTiles(location);
+        List<Location> list = new ArrayList<>(surroundingTiles);
+
+        if(!list.isEmpty() && Math.random() < 0.25){
+            Location new_location = list.get(random.nextInt(list.size()));
+            if(!world.containsNonBlocking(new_location)){
+                world.setTile(new_location, this);
+            }
+        }
+    }
 }
