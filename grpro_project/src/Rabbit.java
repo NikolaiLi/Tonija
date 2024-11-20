7d750b3625e6b76e10ffd2ee289f34e6d6aead65
Nikolai Li <nikolaihcoe@gmail.com>
2024-11-20 14:15:07 +0100

    Rabbit spawner (main) & Rabbit walk (Rabbit)

--- grpro_project/src/Rabbit.java
+++ grpro_project/src/Rabbit.java
@@ -1,2 +1,21 @@
-public class Rabbit {
-}
+import itumulator.simulator.Actor;
+import itumulator.world.World;
+import itumulator.world.Location;
+import java.util.*;
+
+public class Rabbit implements Actor {
+    @Override
+    public void act(World world) {
+        Random r = new Random();
+
+        Set<Location> neighbours = world.getEmptySurroundingTiles();
+        List<Location> list = new ArrayList<>(neighbours);
+
+        if (!neighbours.isEmpty()) {
+            int randomNumber = r.nextInt(list.size());
+            Location l = list.get(randomNumber);
+            world.move(this, l);
+        }
+
+        }
+    }
