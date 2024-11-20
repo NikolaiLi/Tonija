20586d172bdcfb21ab7e8decd1fe386cd7fe31bd
Tore Haugan Engberg <teng@itu.dk>
2024-11-20 13:19:58 +0100

    Building Grass class

--- src/Grass.java
+++ src/Grass.java
@@ -1,2 +1,14 @@
+import itumulator.executable.Program;
+import itumulator.world.Location;
+import itumulator.world.World;
+
+import java.util.*;
+
 public class Grass {
+    Random r = new Random();
+
+    public void grow(World world){
+
+    }
+
 }
