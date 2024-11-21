161bb286076bec434afd24c4058c6b3069502863
Tore Haugan Engberg <teng@itu.dk>
2024-11-21 14:08:50 +0100

    Removed all functions in grass
    Starting over...

--- grpro_project/src/Grass.java
+++ grpro_project/src/Grass.java
@@ -6,38 +6,5 @@ import java.util.*;
 
 public class Grass {
     Random random = new Random();
-
-    public void grow(World world){
-        Set<Location> neighbours = world.getSurroundingTiles();
-        List<Location> list = new ArrayList<>(neighbours);
-
-        if(!list.isEmpty()){
-            Location location = list.get(random.nextInt(list.size()));
-
-            if(world.isTileEmpty(location)){
-                world.setTile(location, this);
-                spread(world, location);
-            }
-        } else {
-            System.out.println("Nothing to grow");
-        }
-    }
-
-    /**
-     * A method that randomly spreads grass objects around the given parameter.
-     * The method has a 25% chance of spreading grass after each step
-     * @param world
-     * @param location
-     */
-    public void spread(World world, Location location){
-        Set<Location> surroundingTiles = world.getSurroundingTiles(location);
-        List<Location> list = new ArrayList<>(surroundingTiles);
-
-        if(!list.isEmpty() /*&& Math.random() < 0.25*/){
-            Location new_location = list.get(random.nextInt(list.size()));
-            if(world.isTileEmpty(new_location)){
-                world.add(new Grass());
-            }
-        }
-    }
+    
 }
