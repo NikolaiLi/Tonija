f5f51f50cc39cbb15ead640b5366ce018c688e9f
Tore Haugan Engberg <teng@itu.dk>
2024-11-21 14:22:09 +0100

    Updated grass
    Added import Actor

--- grpro_project/src/Grass.java
+++ grpro_project/src/Grass.java
@@ -1,14 +1,15 @@
 import itumulator.executable.Program;
+import itumulator.simulator.Actor;
 import itumulator.world.Location;
 import itumulator.world.World;
 
 import java.util.*;
 
-public class Grass implements Actor{
+public class Grass implements Actor {
     Random random = new Random();
 
     @Override
     public void act(World world){
-
+        world.getLocation(this);
     }
 }
