90bb16132e4d6625574e713a5bfc22412ee0a0a7
Tore Haugan Engberg <teng@itu.dk>
2024-11-21 21:09:59 +0100

    Added 'data' folder in project
    Working on the FileReader and how the lines should be handled

--- grpro_project/src/Main.java
+++ grpro_project/src/Main.java
@@ -1,5 +1,7 @@
 import java.awt.Color;
+import java.io.*;
 import java.util.Random;
+
 import itumulator.executable.DisplayInformation;
 import itumulator.executable.Program;
 import itumulator.world.Location;
@@ -11,6 +13,21 @@ public class Main {
         int size = 15;
         int delay = 150;
         int display_size = 800;
+
+
+        //System.out.println("Current Working Directory: " + System.getProperty("user.dir"));
+        String filePath = "data/week-1/t1-1a.txt";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(reader.readLine());
+            }
+            reader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
 
         Program p = new Program(size, display_size, delay);
         World world = p.getWorld();
@@ -25,15 +42,15 @@ public class Main {
         int amountOfRabbits = 3;
         Random r = new Random();
 
-        for(int i = 0; i < amountOfRabbits; i++){
+        for (int i = 0; i < amountOfRabbits; i++) {
             int x = r.nextInt(size);
             int y = r.nextInt(size);
-            Location l = new Location(x,y);
+            Location l = new Location(x, y);
 
-            while(!world.isTileEmpty(l)) {
+            while (!world.isTileEmpty(l)) {
                 x = r.nextInt(size);
                 y = r.nextInt(size);
-                l = new Location(x,y);
+                l = new Location(x, y);
             }
 
             world.setTile(l, new Rabbit());
@@ -45,15 +62,15 @@ public class Main {
         // Spawner Rabbits forskellige steder (Krav 1)
 
         int amountOfGrass = 1;
-        for(int i = 0; i < amountOfGrass; i++){
+        for (int i = 0; i < amountOfGrass; i++) {
             int x = r.nextInt(size);
             int y = r.nextInt(size);
-            Location l = new Location(x,y);
+            Location l = new Location(x, y);
 
-            while(!world.isTileEmpty(l)) {
+            while (!world.isTileEmpty(l)) {
                 x = r.nextInt(size);
                 y = r.nextInt(size);
-                l = new Location(x,y);
+                l = new Location(x, y);
             }
 
             world.setTile(l, new Grass());
