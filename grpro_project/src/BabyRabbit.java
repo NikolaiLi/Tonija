176461c0e26240f2360b6d68b5be088bc5ee534a
Nikolai Li <nikolaihcoe@gmail.com>
2024-11-24 17:40:31 +0100

    Rabbit hiding behaviour in ACT work (without seek)

--- grpro_project/src/BabyRabbit.java
+++ grpro_project/src/BabyRabbit.java
@@ -10,18 +10,36 @@ public class BabyRabbit extends Rabbit {
     }
 
     public void act(World world) {
-        // Henter superklassens act (Rabbit's act)
-        super.act(world);
         if (age == 15) {
             grow(world);
             return;
         }
+
+        // Henter superklassens act (Rabbit's act)
+        super.act(world);
     }
 
     public void grow(World world) {
-        System.out.println("Baby Rabbit has grown up");
-        Location l = world.getLocation(this);
-        world.delete(this);
-        world.setTile(l, new AdultRabbit(getHunger()));
+        if (hiding) {
+            Location rabbitHoleLocation = currentHidingPlace.getLocation();
+            if (world.isTileEmpty(rabbitHoleLocation)) {
+                world.setTile(rabbitHoleLocation, this);
+                world.delete(this);
+                System.out.println("Baby Rabbit has grown up");
+                world.setTile(rabbitHoleLocation, new AdultRabbit(getHunger()));
+            } else {
+                Object o = world.getTile(rabbitHoleLocation);
+                world.remove(o);
+                Rabbit adultRabbit = new AdultRabbit(getHunger());
+                world.setTile(rabbitHoleLocation, adultRabbit);
+                adultRabbit.hide(world);
+                world.setTile(rabbitHoleLocation, o);
+            }
+        } else {
+            System.out.println("Baby Rabbit has grown up");
+            Location l = world.getLocation(this);
+            world.delete(this);
+            world.setTile(l, new AdultRabbit(getHunger()));
+        }
     }
 }
