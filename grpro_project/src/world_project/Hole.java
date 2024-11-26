332b8b77f5b969d655f89aae4bb881f61b9864e8
Nikolai Li <nikolaihcoe@gmail.com>
2024-11-26 15:13:10 +0100

    Hole.java

---
+++ grpro_project/src/world_project/Hole.java
@@ -0,0 +1,8 @@
+package world_project;
+import itumulator.world.Location;
+import itumulator.world.NonBlocking;
+import itumulator.world.World;
+
+public abstract class Hole implements NonBlocking {
+    
+}
