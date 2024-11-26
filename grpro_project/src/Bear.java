b6238bca7ca396a53a446c00925a3b861b8b7d5c
Nikolai Li <nikolaihcoe@gmail.com>
2024-11-26 13:07:05 +0100

    Bear Basics

--- grpro_project/src/Bear.java
+++ grpro_project/src/Bear.java
@@ -1,2 +1,26 @@
-public class Bear {
+import itumulator.world.World;
+import itumulator.world.Location;
+import java.util.*;
+
+public class Bear extends Creature {
+    
+    @Override
+    public void act(World world) {
+        if (!isAlive()) {
+            return;
+        }
+
+
+    }
+
+    @Override
+    public void move(World world) {
+
+    }
+
+    @Override
+    public void eat(World world) {
+
+    }
+
 }
