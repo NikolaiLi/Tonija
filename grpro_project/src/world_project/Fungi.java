7f645bbe03c995f39cad649b2483e45ea3abcc8f
Nikolai Li <nikolaihcoe@gmail.com>
2024-12-05 18:02:51 +0100

    Fungi and Carcass DONE!

--- grpro_project/src/world_project/Fungi.java
+++ grpro_project/src/world_project/Fungi.java
@@ -12,20 +12,23 @@ import java.util.*;
 public class Fungi implements Actor, DynamicDisplayInformationProvider {
     DisplayInformation displaySmall;
     DisplayInformation displayBig;
+    int duration;
     boolean isBig;
 
     public Fungi(boolean isBig) {
         this.isBig = isBig;
         if (isBig) {
             displayBig = new DisplayInformation(Color.yellow, "fungi");
+            duration = 100;
         } else {
             displaySmall = new DisplayInformation(Color.yellow, "fungi-small");
+            duration = 50;
         }
     }
 
     @Override
     public void act(World world) {
-
+        dying();
     }
 
     @Override
@@ -35,5 +38,23 @@ public class Fungi implements Actor, DynamicDisplayInformationProvider {
         } else {
             return displaySmall;
         }
+    }
+
+    public void spread(World world) {
+        Location l = world.getLocation(this);
+        Set<Location> spreadArea = world.getSurroundingTiles(l, 2);
+
+        for (Location location : spreadArea) {
+            Object tile = world.getTile(location);
+            if (tile instanceof Carcass spreadCarcass) {
+                if (!spreadCarcass.getIsInfected()) {
+                    spreadCarcass.getInfected();
+                }
+            }
+        }
+    }
+
+    public void dying() {
+        duration -= 5;
     }
 }
